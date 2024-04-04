0ccff2e24c2f94d67d2353b1bdcafd25c77d7092
Emmanuel Juma <114738453+omondiJuma@users.noreply.github.com>
2024-04-04 22:25:24 +0300

    Update logisticRegression.R

--- logisticRegression.R
+++ logisticRegression.R
@@ -5,3 +5,5 @@ data(wcgs, package="faraway")
 #USING LOGISTIC REGRESION TO DISCOVER PROBABILITY OF DEVELOPING HEART DISEASE
 wcgs_lgs<-glm(chd~arcus+timechd+dibep+cigs+behave+chol+dbp+sdp+weight+height+age, family = binomial(link="logit"), data = wcgs,maxit=4000)
 summary(wcgs_lgs)
+#the results indicate from the small pr(>|z|) values the numbers of cigarettes smokes has an impact on whether one will develop heart disease or not.
+#cholesterol has a minimal to no effect on the probability of developing heart disease due to the high Pr(>|z|) value.
