bd705d322af1598fc12a8c8fae9b8897a12df49a
Emmanuel Juma <114738453+omondiJuma@users.noreply.github.com>
2024-04-04 22:09:19 +0300

    Update logisticRegression.R

--- logisticRegression.R
+++ logisticRegression.R
@@ -2,5 +2,6 @@ install.packages('faraway')
 library(faraway)
 data(wcgs, package="faraway")
 #install necessary packages
-install.packages('caTools')
-library('caTools')
+#USING LOGISTIC REGRESION TO DISCOVER PROBABILITY OF DEVELOPING HEART DISEASE
+wcgs_lgs<-glm(chd~arcus+timechd+dibep+cigs+behave+chol+dbp+sdp+weight+height+age, family = binomial(link="logit"), data = wcgs,maxit=4000)
+summary(wcgs_lgs)
