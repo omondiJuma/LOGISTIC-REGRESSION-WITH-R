535996214beb8f216c8b672635aff99143750dde
Emmanuel Juma <114738453+omondiJuma@users.noreply.github.com>
2024-04-08 20:29:42 +0300

    Update logisticRegression.R

--- logisticRegression.R
+++ logisticRegression.R
@@ -7,5 +7,5 @@ wcgs_lgs<-glm(chd~arcus+timechd+dibep+cigs+behave+chol+dbp+sdp+weight+height+age
 summary(wcgs_lgs)
 #the results indicate from the small pr(>|z|) values the numbers of cigarettes smokes has an impact on whether one will develop heart disease or not.
 #cholesterol has a minimal to no effect on the probability of developing heart disease due to the high Pr(>|z|) value.
-#very variable has a contribution to the development of a heart disease but cigarette smoking is the major one
-.
+#very variable has a contribution to the development of a heart disease but cigarette smoking is the major one.
+
